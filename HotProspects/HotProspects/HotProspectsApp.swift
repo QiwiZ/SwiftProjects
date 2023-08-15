a45d95e8086d680bac64f1cae4f417af5508c1d6
Julian Saxl <julian.saxl@gmail.com>
2023-08-15 11:07:49 -0600

    finish learning day79

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Julian Saxl on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
