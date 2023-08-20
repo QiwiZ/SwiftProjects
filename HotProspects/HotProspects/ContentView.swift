2c3efeaf3a7c3a1251f2abb329326254ccc20876
Julian Saxl <julian.saxl@gmail.com>
2023-08-20 14:42:47 -0600

    finish da 81, reset state to start on day 82

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,46 +7,11 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var name = "Foo"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
-    
+
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-            Text("Tab 2")
-                .onTapGesture {
-                    selectedTab = "One"
-                }
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }.tag("Two")
-        }
+        Text("")
     }
 }
 
---
+++ HotProspects/HotProspects/DelayedUpdater.swift
@@ -0,0 +1,24 @@
+//
+//  DelayedUpdater.swift
+//  HotProspects
+//
+//  Created by Julian Saxl on 2023-08-17.
+//
+
+import Foundation
+
+@MainActor class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
