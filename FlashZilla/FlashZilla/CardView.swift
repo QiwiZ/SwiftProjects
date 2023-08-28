6b4df9cecdeab9ce85fb9814d4ea36cb52ada173
Julian Saxl <julian.saxl@gmail.com>
2023-08-28 13:31:22 -0600

    add differentiation for swiping directions, add timer, finish day 89

--- FlashZilla/FlashZilla/CardView.swift
+++ FlashZilla/FlashZilla/CardView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     let card: Card
     var removal: (() -> Void)? = nil
     @State private var isShowingAnswer = false
@@ -16,7 +17,16 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(differentiateWithoutColor
+                      ? .white
+                      : .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(differentiateWithoutColor
+                            ? nil
+                            : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             VStack {
                 Text(card.prompt)
