62bdd7ee23a7ef744c7a6ca7ee0d39d4ba24f791
Julian Saxl <julian.saxl@gmail.com>
2023-08-28 12:04:43 -0600

    add card view, create example stack of cards and make them draggable, finish day 88

---
+++ FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Julian Saxl on 2023-08-28.
+//
+
+import Foundation
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Example promp", answer: "Example Answer")
+}
