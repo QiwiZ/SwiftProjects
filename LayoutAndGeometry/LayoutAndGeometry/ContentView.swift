6f7d16229165a7b7d636af8f29ffc74500d818b2
Julian Saxl <julian.saxl@gmail.com>
2023-09-04 14:11:18 -0600

    finish day 93, 94

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -17,30 +17,58 @@ extension VerticalAlignment {
     static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("Foo")
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Text("Foo")
-                Image("Example")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                Text("Foo")
-            }
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
 
-            VStack {
-                Text("Full name:")
-                Text("Foo")
-                Text("Foo")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-                Text("Foo")
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        let opacity = (geo.frame(in: .global).minY - 50) / 100
+                        let scaling = max(0.5, geo.frame(in: .global).minY / 700) 
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(opacity)
+                            .scaleEffect(scaling)
+                    }
+                    .frame(height: 40)
+                }
             }
         }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+                    Text("Left")
+                    GeometryReader { geo in
+                        Text("Center")
+                            .background(.blue)
+                            .onTapGesture {
+                                print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                                print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                                print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                            }
+                    }
+                    .background(.orange)
+                    Text("Right")
+                }
     }
 }
 
