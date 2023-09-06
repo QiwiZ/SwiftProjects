d9a1e59cd92e4d83d1f6a320d70ec1fce3aeace2
Julian Saxl <julian.saxl@gmail.com>
2023-09-06 12:01:27 -0600

    finish day 96

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Julian Saxl on 2023-09-06.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
