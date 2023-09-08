83e734b5eaf347e61a74d96bcbc7d2740422c581
Julian Saxl <julian.saxl@gmail.com>
2023-09-08 09:49:50 -0600

    add initial resort views, finish day 97

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,23 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            return resorts
+        } else {
+            return resorts.filter {$0.name.localizedCaseInsensitiveContains(searchText)}
+        }
+        
+    }
 
     var body: some View {
         NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
-        }
-    }
-
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+            List(filteredResorts) { resort in
+                NavigationLink {
+                    ResortView(resort: resort)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    
+                    VStack {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.navigationTitle("Resorts")
+                .searchable(text: $searchText, prompt: "Search for a resort")
+            
+            WelcomeView()
         }
     }
 }
